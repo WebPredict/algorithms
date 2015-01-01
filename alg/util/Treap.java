85449bf232d3d0f077674349f9224872f76e1cfc
Jeff Sanchez <paraglidingjeff@gmail.com>
2015-01-01 17:28:03 +0000

    moved files

--- alg/util/Treap.java
+++
@@ -1,8 +0,0 @@
-package alg.util;
-
-/**
- * Tree-heap combo.
- * allow binary search of keys, min/max heap of values. each key is given a randomly chosen numeric priority which should be heap ordered.
- */
-public class Treap {
-}
